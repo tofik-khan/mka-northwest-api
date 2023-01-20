694646ac1d8085e14b36338e6f84a6b60ba7aa47
Tofik Khan <tofik.khan@mkausa.org>
2023-01-19 19:44:38 -0800

    Add helper functions

--- pages/sms/index.js
+++ pages/sms/index.js
@@ -1,4 +1,7 @@
+import TestHelper from "../helpers/test";
+
 console.log("Testing API structure");
+TestHelper();
 
 export default function SendSMS() {
     return <h1>Hello, from nested folder!</h1>;
