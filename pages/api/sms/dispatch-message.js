2c6bbb3ea86c00ea5e279f2523d0e5543f035968
Tofik Khan <tofik.khan@mkausa.org>
2023-01-26 19:49:33 -0800

    Add API entry-point

--- pages/api/sms/dispatch-message.js
+++ pages/api/sms/dispatch-message.js
@@ -30,6 +30,8 @@ export default async function handler(req, res) {
         return;
     }
 
+    userArray = req.body.users || "None";
+    console.log(userArray);
 
     res.status(200).json({success: true})
 }
