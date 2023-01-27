47efee09f40da424a102a8ed3eb5bdf05018f238
Tofik Khan <tofik.khan@mkausa.org>
2023-01-26 22:06:14 -0800

    Update dispatch-message as a secondary function

--- pages/api/sms/dispatch-message.js
+++ pages/api/sms/dispatch-message.js
@@ -1,37 +1,22 @@
- /**
-  * 
-  * @param {Object} req: payload received from calling application
-  * req.body contains the data payload
-  * req.body = {
-  *     "message": String - unformatted message to be sent to users
-  *     "users": Array - User Array with names and phone numbers
-  *     "auth": String - secret key needed to authenticate API
-  * }
-  * req.body.users = [
-  *     {
-            "_id": {
-                "$oid": "ABC123abc"
-            },
-            "firstname": "Test",
-            "lastname": "Khadim",
-            "phone": "(360) 123-1234",
-            ...
-        },
-        ...
-  * ]
-  * 
-  */
+import sanitizePhoneNumber from "../helpers/sanitize-phonenumber";
+import formatMessage from "../helpers/format-message";
 
-export default async function handler(req, res) {
+export default async function dispatchMessage(userArray, message) {
 
-    // Authenticate request
-    if(!req.body.auth || req.body.auth !== process.env.API_AUTH_TOKEN ) {
-        res.status(401).json({success: false, error: { message: "Failed to Authenticate"}});
-        return;
-    }
+    const accountSid = process.env.TWILIO_ACCOUNT_SID
+    const authToken = process.env.TWILIO_AUTH_TOKEN
 
-    userArray = req.body.users || "None";
-    console.log(userArray);
+    const twilio = require("twilio");
+    const client = new twilio(accountSid, authToken);
 
-    res.status(200).json({success: true})
+    userArray.forEach((element) => {
+
+        client.messages
+        .create({
+            body: formatMessage(message, element),
+            to: sanitizePhoneNumber(element.phone),
+            messagingServiceSid: process.env.TWILIO_SERVICE_SID,
+        })
+
+    });
 }
