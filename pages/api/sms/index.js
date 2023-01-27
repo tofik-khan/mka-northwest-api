91e54a47a3b4039ccdef6803abf75bf370d47157
Tofik Khan <tofik.khan@mkausa.org>
2023-01-26 22:26:25 -0800

    Remove Documentation

--- pages/api/sms/index.js
+++ pages/api/sms/index.js
@@ -1,30 +1,6 @@
 import getAllKhuddam from "../database/get-all-khuddam";
 import dispatchMessage from "./dispatch-message";
 
-
- /**
-  * 
-  * @param {Object} req: payload received from calling application
-  * req.body contains the data payload
-  * req.body = {
-  *     "message": String - unformatted message to be sent to users
-  *     "users": Array - User Array with names and phone numbers
-  *     "auth": String - secret key needed to authenticate API
-  * }
-  * req.body.users = [
-  *     {
-            "_id": {
-                "$oid": "ABC123abc"
-            },
-            "firstname": "Test",
-            "lastname": "Khadim",
-            "phone": "(360) 123-1234",
-            ...
-        },
-        ...
-  * ]
-  * 
-  */
 export default async function handler(req, res) {
 
     // Authenticate request
